5668a1a7cee1bf6aed785cec79f736a7b1089f5c
ksahaban <ksahaban@eecs.wsu.edu>
2019-04-04 16:31:16 -0700

    removed  google button

--- assets/JS/login.js
+++ assets/JS/login.js
@@ -14,7 +14,7 @@
 function onLoadGoogleCallback(){
     gapi.load('auth2', function() {
       auth2 = gapi.auth2.init({
-        client_id: '<YOUR-CLIENT-ID>.apps.googleusercontent.com',
+        client_id: '582903321253-ftsq8n0a06v7qu4snejqqoh2t6qhq6o9.apps.googleusercontent.com',
         cookiepolicy: 'single_host_origin',
         scope: 'profile'
       });
