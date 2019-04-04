c2165358031fd0c4d036b1699b0ea4633440a6e8
ksahaban <ksahaban@eecs.wsu.edu>
2019-04-04 16:15:08 -0700

    gapi

--- assets/JS/login.js
+++ assets/JS/login.js
@@ -1,4 +1,12 @@
-function authenticate()
+function authenticate(googleUser)
 {
     window.location.replace('html/mainpage.html');
+
+    var profile = googleUser.getBasicProfile();
+    console.log(profile);
+    var imageURL = profile.getImageUrl();
+    var emailAddr = profile.getEmail();
+    console.log(imageURL);
+    console.log(emailAddr);
+
 }
