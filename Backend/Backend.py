1c24b2c6f680ee749eeb5f32194c63bc6ef3744b
Paul <Paul.Brennan@wsu.edu>
2019-04-05 21:46:22 -0700

    Fully functional database with foreign keys established and the addTeam function working.

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -13,36 +13,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = sqlalchemy.SQLAlchemy(app)
 
 
+# ******TABLE CREATION***********
 
-
-#******TABLE CREATION***********
-
-#Creating a table for each team, including the primary key
-#teamID, the team name and the manager email
+# Creating a table for each team, including the primary key
+# teamID, the team name and the manager email
 class Teams(db.Model):
     __tablename__ = 'Teams'
-    teamID = db.Column(db.Integer, primary_key=True)                            #???
+    teamID = db.Column(db.Integer, primary_key=True)  # ???
     teamName = db.Column(db.String(50))
     managerEmail = db.Column(db.String(50))
+    players = db.relationship('Players', backref='team')
 
-#Creating a table for each player, including the primary key
-#playerID, the player number, and a reference to the
-#player's teamID
+# Creating a table for each player, including the primary key
+# playerID, the player number, and a reference to the
+# player's teamID
+
+
 class Players(db.Model):
     __tablename__ = 'Players'
     playerID = db.Column(db.Integer, primary_key=True)
-    teamID= db.relationship('Teams', backref = 'Players', lazy = True)          #???   
     playerNumber = db.Column(db.Integer)
+    team_id = db.Column(db.Integer, db.ForeignKey(Teams.teamID))
+    games = db.relationship('Games',backref='player')
+    # teamID= db.relationship('Teams', backref = 'Players', lazy = T rue)          #???
 
-#Creating a table for each game, including foreign key
-#references to a player ID and their corresponding team ID,
-#along with all statistical information such as
-#points, rebounds, assists, shooting numbers, etc.
+
+# Creating a table for each game, including foreign key
+# references to a player ID and their corresponding team ID,
+# along with all statistical information such as
+# points, rebounds, assists, shooting numbers, etc.
+
+
+
+    #playerID = db.relationship('Players', backref='Games', lazy=True)  # ???
+    #teamID = db.relationship('Teams', backref='Games', lazy=True)  # ???
 class Games(db.Model):
     __tablename__ = 'Games'
-    gameID = db.Column(db.Integer, primary_key=True)                        
-    playerID= db.relationship('Players', backref = 'Games', lazy = True)        #???
-    teamID= db.relationship('Teams', backref = 'Games', lazy = True)            #???
+    gameID = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey(Players.playerID))
     points = db.Column(db.Integer)
     FGMade = db.Column(db.Integer)
     FGAttempted = db.Column(db.Integer)
@@ -62,13 +70,30 @@ class Games(db.Model):
     turnovers = db.Column(db.Integer)
     fouls = db.Column(db.Integer)
 
+base_url = '/api/'
 
+
+# ***********Web API routes**************
+
+
+@app.route(base_url+'addTeam', methods=['POST'])
+def addTeam():
+    teamName = request.get_json().get('teamName')
+    managerEmail = request.get_json().get('managerEmail')
+
+    newTeam = Teams(teamName=teamName, managerEmail=managerEmail)
+
+    db.session.add(newTeam)
+    db.session.commit()
+
+    db.session.refresh(newTeam)
+
+    return jsonify({"Team Name": teamName, "Manager": managerEmail, "Success": True}), 200
 
 
 def main():
-    db.create_all()     #Creates the established tables
-    app.run()           #Runs the Flask application
-
+    db.create_all()  # Creates the established tables
+    app.run()  # Runs the Flask application
 
 
 if __name__ == '__main__':
